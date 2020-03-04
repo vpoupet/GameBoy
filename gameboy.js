c789c32a6c8f7f58448f1ec542cef732e2406868
vpoupet <victor.poupet@gmail.com>
2020-03-04 09:40:49 +0100

    wip

--- gameboy.js
+++ gameboy.js
@@ -13,55 +13,15 @@ function int8(x) {
 class GameBoy {
     constructor() {
         /**
-         * Program counter
-         * @type {number}
+         * 8bit registers A F B C D E H L (followed by SP and PC)
+         * @type {Uint16Array}
          */
-        this._pc = 0;
+        this.registers = new Uint8Array(12);
         /**
-         * Stack pointer
-         * @type {number}
+         * 16bit registers AF BC DE HL SP PC
+         * @type {Uint16Array}
          */
-        this._sp = 0;
-        /**
-         * A register
-         * @type {number}
-         */
-        this._a = 0;
-        /**
-         * B register
-         * @type {number}
-         */
-        this._b = 0;
-        /**
-         * C register
-         * @type {number}
-         */
-        this._c = 0;
-        /**
-         * D register
-         * @type {number}
-         */
-        this._d = 0;
-        /**
-         * E register
-         * @type {number}
-         */
-        this._e = 0;
-        /**
-         * F register
-         * @type {number}
-         */
-        this._f = 0;
-        /**
-         * H register
-         * @type {number}
-         */
-        this._h = 0;
-        /**
-         * L register
-         * @type {number}
-         */
-        this._l = 0;
+        this.registers16 = new Uint16Array(this.registers.buffer);
         /**
          * Clock counter
          * @type {number}
@@ -81,12 +41,23 @@ class GameBoy {
             return this.memory[addr];
         }
     }
-
-    mmu_set(addr, val) {
+    /**
+     * Increments a memory value
+     * @param addr
+     * @returns {number} the value before increment
+     */
+    mmu_inc(addr) {
         if (0xE000 <= addr && addr < 0xFE00) {
-            this.memory[addr - 0x2000] = val & 0xFF;
+            return this.memory[addr - 0x2000]++;
         } else {
-            this.memory[addr] = val & 0xFF;
+            return this.memory[addr]++;
+        }
+    }
+    mmu_dec(addr) {
+        if (0xE000 <= addr && addr < 0xFE00) {
+            return this.memory[addr - 0x2000]--;
+        } else {
+            return this.memory[addr]--;
         }
     }
 
@@ -94,141 +65,20 @@ class GameBoy {
         return this.mmu_get(addr) | (this.mmu_get(addr + 1) << 8);
     }
 
+    mmu_set(addr, val) {
+        if (0xE000 <= addr && addr < 0xFE00) {
+            this.memory[addr - 0x2000] = val;
+        } else {
+            this.memory[addr] = val;
+        }
+    }
     mmu_set16(addr, val) {
-        this.mmu_set(addr, val & 0xFF);
-        this.mmu_set(addr + 1, (val >> 8) & 0xFF);
-    }
-
-    cpu_getA() {
-        return this._a;
-    }
-
-    cpu_getB() {
-        return this._b;
-    }
-
-    cpu_getC() {
-        return this._c;
-    }
-
-    cpu_getD() {
-        return this._d;
-    }
-
-    cpu_getE() {
-        return this._e;
-    }
-
-    cpu_getF() {
-        return this._f;
-    }
-
-    cpu_getG() {
-        return this._h;
-    }
-
-    cpu_getL() {
-        return this._l;
-    }
-
-    cpu_setA(val) {
-        this._a = val & 0xFF;
-    }
-
-    cpu_setB(val) {
-        this._b = val & 0xFF;
-    }
-
-    cpu_setC(val) {
-        this._c = val & 0xFF;
-    }
-
-    cpu_setD(val) {
-        this._d = val & 0xFF;
-    }
-
-    cpu_setE(val) {
-        this._e = val & 0xFF;
-    }
-
-    cpu_setF(val) {
-        this._f = val & 0xFF;
-    }
-
-    cpu_setH(val) {
-        this._h = val & 0xFF;
-    }
-
-    cpu_setL(val) {
-        this._l = val & 0xFF;
-    }
-
-    cpu_getSP() {
-        return this._sp;
-    }
-
-    cpu_setSP(val) {
-        this._sp = val & 0xFFFF;
-    }
-
-    cpu_getPC() {
-        return this._pc;
-    }
-
-    cpu_setPC(val) {
-        this._pc = val & 0xFFFF;
-    }
-
-    cpu_getAF() {
-        return this._a | (this._f << 8);
-    }
-
-    cpu_setAF(val) {
-        this._a = val & 0xFF;
-        this._f = (val >> 8) & 0xFF;
-    }
-
-    cpu_getBC() {
-        return this._b | (this._c << 8);
-    }
-
-    cpu_setBC(val) {
-        this._b = val & 0xFF;
-        this._c = (val >> 8) & 0xFF;
-    }
-
-    cpu_getDE() {
-        return this._d | (this._e << 8);
-    }
-
-    cpu_setDE(val) {
-        this._d = val & 0xFF;
-        this._e = (val >> 8) & 0xFF;
-    }
-
-    cpu_getHL() {
-        return this._h | (this._l << 8);
-    }
-
-    cpu_setHL(val) {
-        this._h = val & 0xFF;
-        this._l = (val >> 8) & 0xFF;
-    }
-
-    mmu_get_d8() {
-        const val = this.mmu_get(this._pc);
-        this.cpu_setPC(this._pc + 1);
-        return val;
-    }
-
-    mmu_get_d16() {
-        const val = this.mmu_get(this._pc) | (this.mmu_get(this._pc + 1) << 8);
-        this.cpu_setPC(this._pc + 2);
-        return val;
+        this.mmu_set(addr, val);
+        this.mmu_set(addr + 1, val >> 8);
     }
 
     cpu_execOp() {
-        opCodes[this.mmu_get_d8()].bind(this)();
+        opCodes[this.mmu_get(this.registers16[5]++)].bind(this)();
     }
 }
 
--- mkcodes.py
+++ mkcodes.py
@@ -1,5 +1,41 @@
 import re
 
+
+def c(x):
+    return {
+        'A': 'this.registers[0]',
+        'F': 'this.registers[1]',
+        'B': 'this.registers[2]',
+        'C': 'this.registers[3]',
+        'D': 'this.registers[4]',
+        'E': 'this.registers[5]',
+        'H': 'this.registers[6]',
+        'L': 'this.registers[7]',
+        'AF': 'this.registers16[0]',
+        'BC': 'this.registers16[1]',
+        'DE': 'this.registers16[2]',
+        'HL': 'this.registers16[3]',
+        'HL+': 'this.registers16[3]++',
+        'HL-': 'this.registers16[3]--',
+        'SP': 'this.registers16[4]',
+        'PC': 'this.registers16[5]',
+        'd8': f'this.mmu_get(this.registers16[5]++)',
+        'a8': f'this.mmu_get(this.registers16[5]++)',
+        'd16': f'this.mmu_get(this.registers16[5]++) | (this.mmu_get(this.registers16[5]++) << 8)',
+        'a16': f'this.mmu_get(this.registers16[5]++) | (this.mmu_get(this.registers16[5]++) << 8)',
+        'SET_Z': 'this.registers[1] |= 0b10000000',
+        'RESET_Z': 'this.registers[1] &= 0b01111111',
+        'SET_N': 'this.registers[1] |= 0b01000000',
+        'RESET_N': 'this.registers[1] &= 0b10111111',
+        'SET_H': 'this.registers[1] |= 0b00100000',
+        'RESET_H': 'this.registers[1] &= 0b11011111',
+        'SET_C': 'this.registers[1] |= 0b00010000',
+        'RESET_C': 'this.registers[1] &= 0b11101111',
+        'GET_C': '(this.registers[1] & 0b00100000) << 5',
+        'r8': '(const x = this.mmu_get_d8(), x & 0x80 ? x - 0x100 : x)',
+    }[x]
+
+
 def parseCode(file):
     line1 = file.readline().strip()
     line2 = file.readline().strip()
@@ -25,59 +61,32 @@ def parseCode(file):
 
 
 def ld(data):
-    incr_hl = 0
     lines = []
-
     dest, src = data['args']
 
-    if src == 'd16':
-        source = 'this.mmu_get_d16()'
-    elif src == 'd8':
-        source = 'this.mmu_get_d8()'
-    elif src == '(a8)':
-        source = 'this.mmu_get(0xFF00 | this.mmu_get_d8())'
-    elif src == '(a16)':
-        source = 'this.mmu_get(this.mmu_get_d16())'
-    elif src == 'SP':
-        source = 'this._sp'
-    elif src == 'SP+r8':
-        source = 'this.mmu_get((this._sp + int8(this.mmu_get_d8()) & 0xFFFF)'
-    elif src.startswith('(HL'):
-        source = f'this.mmu_get(this.cpu_getHL())'
-        if src[3] == '+':
-            incr_hl = 1
-        elif src[3] == '-':
-            incr_hl = -1
+    if src == 'SP+r8':
+        lines.append(f'const sp = {c("SP")};')
+        lines.append(f'const r8 = {c("r8")};')
+        lines.append(f'{c("RESET_Z")};')
+        lines.append(f'{c("RESET_N")};')
+        lines.append(f'(sp & 0x0F + r8 & 0x0F) & 0xF0 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(sp & 0xFF + r8 & 0xFF) & 0x0F00 ? {c("SET_C")} : {c("RESET_C")};')
+        source = f'this.mmu_get16((sp + r8) & 0xFFFF)'
     elif src.startswith('('):
-        if len(src) == 3:
-            source = f'this.mmu_get(0xFF00 | this._{src[1].lower()})'
-        elif len(src) == 4:
-            source = f'this.mmu_get(this.cpu_get{src[1:3]}())'
+        if src == '(a8)' or len(src) == 3:
+            source = f'this.mmu_get(0xFF00 | {c(src[1:-1])})'
+        else:
+            source = f'this.mmu_get({c(src[1:-1])})'
     else:
-        source = f'this.cpu_get{src}()'
+        source = c(src)
 
-    if dest == '(a8)':
-        lines.append(f'this.mmu_set(0xFF00 | this.mmu_get_d8(), {source});')
-    elif dest == '(a16)':
-        lines.append(f'this.mmu_set(this.mmu_get_d8() | (this.mmu_get_d8() << 8), {source});')
-    elif dest.startswith('(HL'):
-        lines.append(f'this.mmu_set(this.cpu_getHL(), {source});')
-        if dest[3] == '+':
-            incr_hl = 1
-        elif dest[3] == '-':
-            incr_hl = -1
-    elif dest.startswith('('):
-        if len(dest) == 3:
-            lines.append(f'this.mmu_set(0xFF00 | this._{dest[1].lower()}, {source});')
-        elif len(dest) == 4:
-            lines.append(f'this.mmu_set(this.cpu_get{dest[1:3]}(), {source});')
+    if dest.startswith('('):
+        if dest == '(a8)' or len(dest) == 3:
+            lines.append(f'this.mmu_set(0xFF00 | {c(dest[1:-1])}, {source});')
+        else:
+            lines.append(f'this.mmu_set({c(dest[1:-1])}, {source});')
     else:
-        lines.append(f'this.cpu_set{dest}({source});')
-
-    if incr_hl == 1:
-        lines.append('this.cpu_setHL(this.cpu_getHL() + 1);')
-    elif incr_hl == -1:
-        lines.append('this.cpu_setHL(this.cpu_getHL() - 1);')
+        lines.append(f'{c(dest)} = {source};')
 
     return lines
 
@@ -86,17 +95,14 @@ def inc(data):
     lines = []
     dest, = data['args']
     if len(dest) == 2:
-        lines.append(f'this.cpu_set{dest}(this.cpu_get{dest} + 1);')
+        lines.append(f'{c(dest)}++;')
     else:
         if dest == '(HL)':
-            lines.append('const addr = this.cpu_getHL();')
-            lines.append('const v = this.mmu_get(addr);')
-            lines.append('this.mmu_set(addr, v + 1);')
+            lines.append(f'const v = this.mmu_inc({c("HL")});')
         else:
-            lines.append(f'const v = this.cpu_get{dest};')
-            lines.append(f'this.cpu_set{dest}(v + 1);')
-        lines.append('if (v & 0xF === 0xF) this.cpu_setFlagH();')
-        lines.append('if (v === 0xFF) this.cpu_setFlagZ();')
+            lines.append(f'const v = {c(dest)}++;')
+        lines.append(f'(v & 0x0F + 1) & 0xF0 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(v + 1) & 0xF00 ? {c("SET_C")} : {c("RESET_C")};')
     return lines
 
 
@@ -104,17 +110,53 @@ def dec(data):
     lines = []
     dest, = data['args']
     if len(dest) == 2:
-        lines.append(f'this.cpu_set{dest}(this.cpu_get{dest} - 1);')
+        lines.append(f'{c(dest)}--;')
     else:
         if dest == '(HL)':
-            lines.append('const addr = this.cpu_getHL();')
-            lines.append('const v = this.mmu_get(addr);')
-            lines.append('this.mmu_set(addr, v - 1);')
+            lines.append(f'const v = this.mmu_dec({c("HL")});')
         else:
-            lines.append(f'const v = this.cpu_get{dest};')
-            lines.append(f'this.cpu_set{dest}(v - 1);')
-        lines.append('if (v & 0xF === 0) this.cpu_setFlagH();')
-        lines.append('if (v === 0x01) this.cpu_setFlagZ();')
+            lines.append(f'const v = {c(dest)}--;')
+        lines.append(f'(v & 0x0F - 1) & 0xF0 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(v - 1) & 0xF00 ? {c("SET_C")} : {c("RESET_C")};')
+    return lines
+
+
+def add(data):
+    lines = []
+    cmd = data['cmd']
+    a, b = data['args']
+    lines.append(f'const a = {c(a)};')
+    if b == '(HL)':
+        lines.append(f'const b = this.mmu_get({c("HL")});')
+    else:
+        lines.append(f'const b = {c(b)};')
+
+    if cmd == 'ADC':
+        lines.append(f'const c = {c("GET_C")};')
+        lines.append(f'{c(a)} += b + c;')
+    else:
+        lines.append(f'{c(a)} += b;')
+
+    # Z flag
+    if a == 'SP':
+        lines.append(f'{c("RESET_Z")};')
+    elif a == 'A':
+        lines.append(f'{c(a)} ? {c("RESET_Z")} : {c("SET_Z")};')
+    # N flag
+    lines.append(f'{c("RESET_N")};')
+    # H and C flags
+    if a == 'HL':
+        lines.append(f'(a & 0x0FFF + b & 0x0FFF) & 0xF000 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(a + b) & 0xF0000 ? {c("SET_C")} : {c("RESET_C")};')
+    elif a == 'SP':
+        lines.append(f'(a & 0x0F + b & 0x0F) & 0xF0 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(a & 0xFF + b) & 0xF00 ? {c("SET_C")} : {c("RESET_C")};')
+    elif cmd == 'ADC':
+        lines.append(f'(a & 0x0F + b & 0x0F + 1) & 0x00F0 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(a + b + 1) & 0xF00 ? {c("SET_C")} : {c("RESET_C")};')
+    else:
+        lines.append(f'(a & 0x0F + b & 0x0F) & 0x00F0 ? {c("SET_H")} : {c("RESET_H")};')
+        lines.append(f'(a + b) & 0xF00 ? {c("SET_C")} : {c("RESET_C")};')
     return lines
 
 
@@ -123,13 +165,19 @@ def make_instructions():
     datas = [parseCode(file) for _ in range(256)]
     output = []
     for data in datas:
+        cmd = data['cmd']
         print(data["line1"], data["line2"], data["line3"])
         lines = [
             f'// {data["line1"]}',
             f'// {data["line2"]}',
             f'// {data["line3"]}']
-        if data['cmd'].startswith('LD'):
+        if cmd in ['LD', 'LDH']:
             lines += ld(data)
+        elif cmd == 'INC':
+            lines += inc(data)
+        elif cmd == 'DEC':
+            lines += dec(data)
+
         if "clock" in data:
             lines.append(f'this.clock += {data["clock"]};')
         output.append('\n'.join(lines))
